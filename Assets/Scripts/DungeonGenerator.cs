57d01d7a51d663bc915d78c5822c6fbf3367f06e
Stan Oosterdijk <sj.oosterdijk@hotmail.com>
2025-03-06 15:24:56 +0100

    Working dungeon splitting

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -7,25 +7,16 @@ using Random = UnityEngine.Random;
 
 public class DungeonGenerator : MonoBehaviour
 {
+    [SerializeField] private Vector2Int _startRoomSize;
+    [SerializeField] private Vector2Int _minimumRoomSize;
+    
     private List<RectInt> _rects = new();
-    private int _roomSplits = 5;
+    
     
     private void Start()
     {
-        var rect1 = new RectInt(new Vector2Int(0,0), new Vector2Int(20,20));
+        var rect1 = new RectInt(new Vector2Int(0,0), _startRoomSize);
         _rects.Add(rect1);
-    }
-    
-    [Button(enabledMode: EButtonEnableMode.Always)]
-    private void SplitRoom()
-    {
-        bool x = Random.Range(0, 2) != 0;
-        int randomRect = Random.Range(0, _rects.Count);
-            
-        if(x)
-            SplitRectX(_rects[randomRect]);
-        else
-            SplitRectY(_rects[randomRect]);
     }
 
     private void Update()
@@ -34,11 +25,54 @@ public class DungeonGenerator : MonoBehaviour
             AlgorithmsUtils.DebugRectInt(rect, Color.red);
     }
 
+    [Button(enabledMode: EButtonEnableMode.Always)]
+    private IEnumerator GenerateDungeon()
+    {
+        yield return StartCoroutine(GenerateRoom());
+        SoundManager.PlaySound("ding");
+    }
+    
+    private IEnumerator GenerateRoom()
+    {
+        bool x = Random.Range(0, 2) != 0;
+
+        RectInt selected = new();
+        foreach (RectInt rect in _rects)
+            if(rect.size.x > selected.width && rect.size.x > selected.height || rect.size.y > selected.width && rect.size.y > selected.height)
+                selected = rect;
+        
+        if (selected.width * 0.5f < _minimumRoomSize.x && selected.height * 0.5f < _minimumRoomSize.y)
+        {
+            Debug.Log(selected.width);
+            Debug.Log(selected.height);
+            yield break;
+        }
+            
+        
+        if(selected.width > selected.height)
+            SplitRectX(selected);
+        else
+            SplitRectY(selected);
+
+        yield return new WaitForSeconds(0.2f);
+        yield return StartCoroutine(GenerateRoom());
+    }
+    
+    [Button(enabledMode: EButtonEnableMode.Always)]
+    private void ResetRooms()
+    {
+        _rects.Clear();
+        _rects.Add(new RectInt(new Vector2Int(0,0), _startRoomSize));
+    }
+
     private void SplitRectX(RectInt rect)
     {
         int width = rect.width;
-        
-        int random = (int)Random.Range(width * 0.2f, width * 0.8f);
+
+        int random = 0;
+        while (_minimumRoomSize.x > random)
+            random = (int)Random.Range(width * 0.3f, width * 0.7f);
+
         Vector2Int position = rect.position;
         
         // Base Rect
@@ -54,7 +88,10 @@ public class DungeonGenerator : MonoBehaviour
     {
         int height = rect.height;
         
-        int random = (int)Random.Range(height * 0.2f, height * 0.8f);
+        int random = 0;
+        while (_minimumRoomSize.y > random)
+            random = (int)Random.Range(height * 0.3f, height * 0.7f);
+        
         Vector2Int position = rect.position;
         
         // Base Rect
